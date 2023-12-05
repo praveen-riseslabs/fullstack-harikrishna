1eaf02c80c084d6c288b43e0f8e140ccd6530388
Harikrishnan14 <harikrishnankhd09@gmail.com>
2023-12-05 15:28:03 +0530

    Navbar and DragAndDrop Page

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ import {
 import Login from './components/Login/Login';
 import Dashboard from './components/Dashboard/Dashboard';
 import ForgotPassword from './components/ForgotPassword/ForgotPassword';
+import DragandDrop from './components/DragandDrop/DragandDrop';
 
 function App() {
   return (
@@ -20,6 +21,7 @@ function App() {
           <Route exact path="/login" element={<Login />}></Route>
           <Route exact path="/forgot-password" element={<ForgotPassword />}></Route>
           <Route exact path="/dashboard" element={<Dashboard />}></Route>
+          <Route exact path="/drag-drop" element={<DragandDrop />}></Route>
         </Routes>
       </Router>
     </div>
---
+++ src/components/DragandDrop/DragandDrop.js
@@ -0,0 +1,48 @@
+import React, { useState } from 'react'
+import Navbar from '../Navbar/Navbar'
+import './style.css'
+
+const DragandDrop = () => {
+
+    const [widgets, setWidgets] = useState([]);
+
+    const handleOnDrag = (e, widgetType) => {
+        e.dataTransfer.setData("widgetType", widgetType);
+    }
+
+    const handleOnDrop = (e) => {
+        const widgetType = e.dataTransfer.getData("widgetType");
+        // console.log("widgetType", widgetType)
+        setWidgets([...widgets, widgetType])
+    }
+
+
+    const handleDragOver = (e) => {
+        e.preventDefault();
+    }
+
+    return (
+        <div>
+            <Navbar />
+            <div className="d-flex">
+                <div className="p-2 cont" style={{ background: "#454545" }} onDrop={handleOnDrop} onDragOver={handleDragOver}>
+                    {widgets.map((widget, index) => {
+                        return <div className="dropped-widget" key={index}>
+                            <button type="button" className={`btn-${widget.toLowerCase(0)}`}>{widget}</button>
+                        </div>
+                    }) }
+                </div>
+                <div className="p-2 cont2">
+                    <h1>Buttons</h1>
+                    <button type="button" className="btn-first" draggable onDragStart={(e) => handleOnDrag(e, "First")}>First</button>
+                    <button type="button" className="btn-second" draggable onDragStart={(e) => handleOnDrag(e, "Second")}>Second</button>
+                    <button type="button" className="btn-third" draggable onDragStart={(e) => handleOnDrag(e, "Third")}>Third</button>
+                    <button type="button" className="btn-fourth" draggable onDragStart={(e) => handleOnDrag(e, "Fourth")}>Fourth</button>
+                    <button type="button" className="btn-fifth" draggable onDragStart={(e) => handleOnDrag(e, "Fifth")}>Fifth</button>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default DragandDrop
