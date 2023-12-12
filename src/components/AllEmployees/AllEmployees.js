f5c53ac75fcf890a1ddf2fb4da1484fc8e0fe70b
Harikrishnan14 <harikrishnankhd09@gmail.com>
2023-12-12 16:38:32 +0530

    Added search feature

--- src/components/AllEmployees/AllEmployees.js
+++ src/components/AllEmployees/AllEmployees.js
@@ -11,6 +11,7 @@ const AllEmployees = () => {
     const [employee, setEmployee] = useState([]);
     const [total, setTotal] = useState(0);
     const [activePage, setActivePage] = useState(1);
+    const [search, setSearch] = useState('');
 
     useEffect(() => {
         fetchData();
@@ -25,7 +26,7 @@ const AllEmployees = () => {
             }
         })
             .then(({ data }) => {
-                console.log(data)
+                // console.log(data)
                 setActivePage(activePage + 1)
                 setEmployee([...employee, ...data.allEmployees])
                 setTotal(data.total)
@@ -39,7 +40,15 @@ const AllEmployees = () => {
             <div className="contents">
                 <Sidebar />
                 <div className="right" style={{ height: "100%" }}>
-                    <h1 className='text-center'>Employees List</h1>
+                    <h1 className='text-center' style={{ marginBottom: "15px" }}>Employees List</h1>
+                    <form className="container-fluid" style={{ display: "flex", padding: "0", justifyContent: "center", marginBottom: "15px" }}>
+                        <div className="input-group" style={{ width: "50%" }}>
+                            <span className="input-group-text" id="basic-addon1" style={{ background: "#282c34", color: "white", border: "1px solid #858585" }}>
+                                <i className="fa-solid fa-magnifying-glass"></i>
+                            </span>
+                            <input type="text" className="form-control" placeholder="" aria-label="search" aria-describedby="basic-addon1" style={{ background: "#282c34", color: "white", border: "1px solid #858585" }} onChange={(e) => {setSearch(e.target.value)}} />
+                        </div>
+                    </form>
                     <div className="third" >
                         <InfiniteScroll dataLength={employee.length} next={fetchData} hasMore={employee.length < total} >
                             <table className="table-dark">
@@ -53,7 +62,9 @@ const AllEmployees = () => {
                                     </tr>
                                 </thead>
                                 <tbody>
-                                    {employee.map((item, _id) => (
+                                    {employee.filter((item) => {
+                                        return search.toLowerCase() === '' ? item : item.emp_name.toLowerCase().includes(search);
+                                    }).map((item, _id) => (
                                         <tr key={_id}>
                                             <th scope="row" style={{ fontWeight: "400" }}>{item.emp_name}</th>
                                             <td style={{ fontWeight: "400" }}>{item.emp_id}</td>
