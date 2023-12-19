40bb13adc3a0e08768e067bdb7955a0dad91b2b1
Harikrishnan14 <harikrishnankhd09@gmail.com>
2023-12-19 23:34:26 +0530

    Sending and Fetching messages to the Frontend

---
+++ src/components/ScrollabelChat.js
@@ -0,0 +1,36 @@
+import React from 'react'
+import ScrollableFeed from 'react-scrollable-feed'
+import { isLastMessage, isSameSender, isSameSenderMargin, isSameUser } from '../config/ChatLogics'
+import { ChatState } from '../Context/ChatProvider'
+import { Avatar, Tooltip } from '@chakra-ui/react'
+
+const ScrollabelChat = ({ messages }) => {
+
+    const { user } = ChatState();
+    return (
+        <ScrollableFeed>
+            {messages && messages.map((msg, index) => (
+                <div style={{ display: "flex" }} key={msg._id}>
+                    {(isSameSender(messages, msg, index, user._id)
+                        || isLastMessage(messages, msg, index, user._id)
+                    ) && (
+                            <Tooltip label={msg.sender.name} placement='bottom-start' hasArrow>
+                                <Avatar mt="7px" mr={1} size="sm" cursor="pointer" name={msg.sender.name} />
+                            </Tooltip>
+                        )}
+                    <span style={{
+                        backgroundColor: `${msg.sender._id === user._id ? "#BEE3F8" : "#B9F5D0"}`,
+
+                        borderRadius: "20px",
+                        padding: "5px 15px",
+                        maxWidth: "75%",
+                        marginLeft: isSameSenderMargin(messages, msg, index, user._id),
+                        marginTop: isSameUser(messages, msg, index, user._id) ? 3 : 10
+                    }}> {msg.content} </span>
+                </div>
+            ))}
+        </ScrollableFeed>
+    )
+}
+
+export default ScrollabelChat
--- src/components/SingleChat.js
+++ src/components/SingleChat.js
@@ -1,13 +1,88 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { ChatState } from '../Context/ChatProvider'
-import { Box, IconButton, Text } from '@chakra-ui/react';
+import { Box, FormControl, IconButton, Input, Spinner, Text, Toast } from '@chakra-ui/react';
 import { ArrowBackIcon } from '@chakra-ui/icons';
 import { getSender } from '../config/ChatLogics';
 import UpdateGroupChatModal from './UpdateGroupChatModal';
+import axios from 'axios';
+import ScrollabelChat from './ScrollabelChat';
 
 const SingleChat = ({ fetchAgain, setFetchAgain }) => {
 
     const { selectedChat, setSelectedChat, user } = ChatState();
+
+    const [messages, setMessages] = useState([]);
+    const [loading, setLoading] = useState(false);
+    const [newMessage, setNewMessage] = useState("");
+
+    const fetchMessages = async () => {
+        if (!selectedChat) return;
+
+        try {
+            const config = {
+                headers: {
+                    Authorization: `Bearer ${user.token}`,
+                },
+            };
+
+            setLoading(true)
+
+            const { data } = await axios.get(`http://localhost:5000/api/message/${selectedChat._id}`, config);
+            // console.log(messages);
+
+            setMessages(data)
+            setLoading(false)
+        } catch (error) {
+            Toast({
+                title: "Error Occured!",
+                description: "Failed to Load the Messages",
+                status: "error",
+                duration: 5000,
+                isClosable: true,
+                position: "top-center",
+            });
+        }
+    }
+
+    useEffect(() => {
+        fetchMessages()
+        // eslint-disable-next-line
+    }, [selectedChat])
+
+    const sendMessage = async (event) => {
+        if (event.key === "Enter" && newMessage) {
+            try {
+                const config = {
+                    headers: {
+                        "Content-type": "application/json",
+                        Authorization: `Bearer ${user.token}`,
+                    }
+                };
+
+                setNewMessage("")
+                const { data } = await axios.post("http://localhost:5000/api/message", {
+                    content: newMessage,
+                    chatId: selectedChat,
+                }, config);
+                console.log(data);
+
+                setMessages([...messages, data])
+            } catch (error) {
+                Toast({
+                    title: "Error Occured!",
+                    description: "Failed to send the Message",
+                    status: "error",
+                    duration: 5000,
+                    isClosable: true,
+                    position: "top-center",
+                });
+            }
+        }
+    }
+
+    const typingHandler = (e) => {
+        setNewMessage(e.target.value)
+    }
 
     return (
         <>
@@ -33,19 +108,28 @@ const SingleChat = ({ fetchAgain, setFetchAgain }) => {
                         ) : (
                             <>
                                 {selectedChat.chatName.toUpperCase()}
-                                <UpdateGroupChatModal fetchAgain={fetchAgain} setFetchAgain={setFetchAgain} />
+                                <UpdateGroupChatModal fetchAgain={fetchAgain} setFetchAgain={setFetchAgain}
+                                    fetchMessages={fetchMessages} />
                             </>
                         )}
                     </Text>
                     <Box display='flex'
-                    flexDir='column'
-                    p={3}
-                    bg="#E8E8E8"
-                    w="100%"
-                    h="100%"
-                    borderRadius="lg"
-                    overflowY="hidden">
-
+                        flexDir='column'
+                        justifyContent="flex-end"
+                        p={3}
+                        bg="#E8E8E8"
+                        w="100%"
+                        h="100%"
+                        borderRadius="lg"
+                        overflowY="hidden">
+                        {loading ? <Spinner size="xl" w={20} h={20} alignSelf="center" margin="auto" /> : (
+                            <div style={{ display: "flex", flexDirection: "column", overflowY: "auto", scrollbarWidth: "none" }}>
+                                <ScrollabelChat messages={messages} />
+                            </div>
+                        )}
+                        <FormControl onKeyDown={sendMessage} id="first-name" isRequired mt={3} display='flex' flexDirection='column-reverse' >
+                            <Input variant="filled" bg="#E0E0E0" alignItems='bottom' placeholder="Enter a message.." value={newMessage} onChange={typingHandler} />
+                        </FormControl>
                     </Box>
                 </>) : (
                 <Box display="flex" alignItems="center" justifyContent="center" h="100%">
